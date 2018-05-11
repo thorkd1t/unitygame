6206c8c995deff731057fdd7f5ec29c5404ad1c8
thorkd1t <paynet1986@gmail.com>
2018-05-11 04:52:21 +0100

    Add files via upload

---
+++ charstarter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+public class charstarter : MonoBehaviour {
+	public GameObject pl;
+	// Use this for initialization
+	void Start () {
+		Instantiate(pl);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+ }
